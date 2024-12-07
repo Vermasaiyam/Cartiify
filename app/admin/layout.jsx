2aa6490d3fc3bae45339c6cc7a98aa2b56943759
Vermasaiyam <vermasaiyam9@gmail.com>
2024-12-07 18:22:13 +0530

    dashboard header

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,13 +1,17 @@
 "use-client"
 
 import Sidebar from "./components/Sidebar"
+import Header from "./components/Header"
 
 export default function Layout({children}){
     return (
         <main className="flex">
             <Sidebar/>
-            <section className="flex-1">
-                {children}
+            <section className="flex-1 flex flex-col">
+                <Header/>
+                <section className="flex-1 bg-[#eff3f4]">
+                    {children}
+                </section>
             </section>
         </main>
     )
