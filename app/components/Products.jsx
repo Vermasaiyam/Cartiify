8d58d94686f5ce28720a994c4f51780200f2c58b
Vermasaiyam <vermasaiyam9@gmail.com>
2024-12-12 22:04:23 +0530

    products page

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -11,9 +11,9 @@ import { Heart } from "lucide-react";
 export default function ProductsGridView({ products }) {
     return (
         <section className="w-full flex justify-center">
-            <div className="flex flex-col gap-5 w-[85%] p-5">
+            <div className="flex flex-col gap-5 md:w-[80%] w-[95%] p-5">
                 <h1 className="text-center font-semibold text-lg">Products</h1>
-                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 w-full">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 w-full">
                     {products?.map((item) => {
                         return <ProductCard product={item} key={item?.id} />;
                     })}
