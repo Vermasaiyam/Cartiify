8b8d79b6a07aba29e1768c1a67dddc92ca2b75a4
Vermasaiyam <vermasaiyam9@gmail.com>
2024-12-13 21:24:10 +0530

    favourite button

--- app/components/FavoriteButton.jsx
+++ app/components/FavoriteButton.jsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useAuth } from "@/contexts/AuthContext";
-// import { useUser } from "@/lib/firestore/user/read";
-// import { updateFavorites } from "@/lib/firestore/user/write";
+import { useUser } from "@/lib/firestore/user/read";
+import { updateFavorites } from "@/lib/firestore/user/write";
 import { Button } from "@nextui-org/react";
 import { useState } from "react";
 import toast from "react-hot-toast";
@@ -12,48 +12,47 @@ import { useRouter } from "next/navigation";
 
 export default function FavoriteButton({ productId }) {
     const { user } = useAuth();
-    // const { data } = useUser({ uid: user?.uid });
+    const { data } = useUser({ uid: user?.uid });
     const [isLoading, setIsLoading] = useState(false);
     const router = useRouter();
 
-    const handlClick = async () => {
+    const handleClick = async () => {
         setIsLoading(true);
-        // try {
-        //     if (!user?.uid) {
-        //         router.push("/login");
-        //         throw new Error("Please Logged In First!");
-        //     }
-        //     if (data?.favorites?.includes(productId)) {
-        //         const newList = data?.favorites?.filter((item) => item != productId);
-        //         await updateFavorites({ list: newList, uid: user?.uid });
-        //     } else {
-        //         await updateFavorites({
-        //             list: [...(data?.favorites ?? []), productId],
-        //             uid: user?.uid,
-        //         });
-        //     }
-        // } catch (error) {
-        //     toast.error(error?.message);
-        // }
+        try {
+            if (!user?.uid) {
+                router.push("/login");
+                throw new Error("Please Logged In First!");
+            }
+            if (data?.favorites?.includes(productId)) {
+                const newList = data?.favorites?.filter((item) => item != productId);
+                await updateFavorites({ list: newList, uid: user?.uid });
+            } else {
+                await updateFavorites({
+                    list: [...(data?.favorites ?? []), productId],
+                    uid: user?.uid,
+                });
+            }
+        } catch (error) {
+            toast.error(error?.message);
+        }
         setIsLoading(false);
     };
 
-    // const isLiked = data?.favorites?.includes(productId);
+    const isLiked = data?.favorites?.includes(productId);
 
     return (
         <Button
             isLoading={isLoading}
             isDisabled={isLoading}
-            onClick={handlClick}
+            onClick={handleClick}
             variant="light"
             color="danger"
             className="rounded-full"
             isIconOnly
             size="sm"
         >
-            {/* {!isLiked && <FavoriteBorderOutlinedIcon fontSize="small" />}
-            {isLiked && <FavoriteIcon fontSize="small" />} */}
-            {<FavoriteBorderOutlinedIcon fontSize="small" />}
+            {!isLiked && <FavoriteBorderOutlinedIcon fontSize="small" />}
+            {isLiked && <FavoriteIcon fontSize="small" />}
         </Button>
     );
 }
