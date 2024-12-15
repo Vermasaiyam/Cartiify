7a5bf1113cb466929015994a92402cbba88831d6
Vermasaiyam <vermasaiyam9@gmail.com>
2024-12-15 19:07:08 +0530

    add review feature added

--- app/(pages)/products/[productId]/page.jsx
+++ app/(pages)/products/[productId]/page.jsx
@@ -2,8 +2,8 @@ import { getProduct } from "@/lib/firestore/products/read_server";
 import Photos from "./components/Photos";
 import Details from "./components/Details";
 import RelatedProducts from "./components/RelatedProducts";
-// import Reviews from "./components/Reviews";
-// import AddReview from "./components/AddReiveiw";
+import Reviews from "./components/Reviews";
+import AddReview from "./components/AddReview";
 import AuthContextProvider from "@/contexts/AuthContext";
 
 export async function generateMetadata({ params }) {
@@ -33,8 +33,8 @@ export default async function Page({ params }) {
             <div className="flex justify-center py-10">
                 <AuthContextProvider>
                     <div className="flex flex-col md:flex-row gap-4 md:max-w-[900px] w-full">
-                        {/* <AddReview productId={productId} />
-                        <Reviews productId={productId} /> */}
+                        <AddReview productId={productId} />
+                        <Reviews productId={productId} />
                     </div>
                 </AuthContextProvider>
             </div>
